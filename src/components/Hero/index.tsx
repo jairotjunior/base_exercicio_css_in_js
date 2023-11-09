b7157dd0d2f71e579ede5d60da37cd27b7e214be
Jairo TJ <jairotjunior@hotmail.com>
2023-11-09 18:34:49 -0300

    Finalizado Conversão

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,13 @@
+import { Container } from '../../styles'
 import { Banner, TituloBanner } from './styles'
 
 const Hero = () => (
   <Banner>
-    <div className="container">
+    <Container>
       <TituloBanner>
         As melhores vagas para tecnologia, design e artes visuais.
       </TituloBanner>
-    </div>
+    </Container>
   </Banner>
 )
 
