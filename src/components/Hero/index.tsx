226f08b41e87bb07b4eefc43bb3b499b670ab042
Jairo TJ <jairotjunior@hotmail.com>
2023-11-09 16:59:57 -0300

    Realizado algumas conversões de .css para styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { Banner, TituloBanner } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <Banner>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <TituloBanner>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      </TituloBanner>
     </div>
-  </form>
+  </Banner>
 )
 
 export default Hero
